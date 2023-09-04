faf36b7cbd6ce1fe52b5b8e87539db7eaceb349c
vilmosvegvari <vegvari.vili@gmail.com>
2023-09-04 12:03:10 +0200

    scaffolding components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', loadComponent: () => import('./login/login.component') },
+  { path: 'game', loadComponent: () => import('./game/game.component') },
+  { path: '', pathMatch: 'full', redirectTo: 'game' },
+];
---
+++ src/app/game/game.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-game',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.scss'],
+})
+export default class GameComponent {}
