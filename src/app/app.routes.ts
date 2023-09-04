79037994825ee91982cb05ef3c27d90c939bf3fa
vilmosvegvari <vegvari.vili@gmail.com>
2023-09-04 17:33:41 +0200

    added missing auth guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,22 @@
-import { Routes } from '@angular/router';
+import { Router, Routes } from '@angular/router';
+import { map } from 'rxjs';
+import { inject } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { selectActive } from './+state/user.selectors';
 
 export const routes: Routes = [
   { path: 'login', loadComponent: () => import('./login/login.component') },
-  { path: 'game', loadComponent: () => import('./game/game.component') },
+  {
+    path: 'game',
+    loadComponent: () => import('./game/game.component'),
+    canActivate: [
+      () => {
+        const router = inject(Router);
+        return inject(Store)
+          .select(selectActive)
+          .pipe(map((e) => (e !== null ? true : router.parseUrl('login'))));
+      },
+    ],
+  },
   { path: '', pathMatch: 'full', redirectTo: 'login' },
 ];
